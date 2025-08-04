2c4d7154f6f7eafb42be08f0fe3e4206bfa835f4
Kaustubh <kaustubh@intellectsoftwares.com>
2025-08-04 16:52:35 +0530

    Linq

---
+++ Linq/Model/DbModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Model
+{
+    class BrokerLogInDump
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? BrokerId { get; set; }
+        public string? JsonDump { get; set; }
+        public string? Token { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? ModifiedBy { get; set; }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            conn.Open();
+            Console.WriteLine("Connected to database!");
+
+        }
+    }
+}
